7199d8f81638cd3e3cb4ac5169e16fe6f54cb705
Gregory Casanova <gregory.casanova@alumnos.usm.cl>
2021-06-26 13:44:01 -0400

    Proyecto finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  if ($(window).width() < 768) {
+    $('.nav-bar__menu').hide();
+    $( '.toggle-button' ).prop( "checked", false );
+  }
+});
+
+$(window).resize(function() {
+  if ($(window).width() < 768) {
+    $('.nav-bar__menu').hide();
+    $( '.toggle-button' ).prop( "checked", false );
+    $( '.toggle-button' ).on( 'click', function() {
+        if( $(this).is(':checked') ){
+            $('.nav-bar__menu').slideDown('slow');
+        } else {
+            $('.nav-bar__menu').slideUp('slow');
+        }
+    });
+  } else {
+    $('.nav-bar__menu').show();
+  }
+});
